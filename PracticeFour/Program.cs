5076dcee18e6e1ccb420a22284d0a19a313c02b4
PSV <pavshin@mail.ru>
2022-02-13 10:11:45 +0700

    Task 28 - Solved

--- PracticeFour/Program.cs
+++ PracticeFour/Program.cs
@@ -23,3 +23,14 @@ double exp(int basis, int extent){
 Console.WriteLine($"Введены значения А = {a} и В = {b}");
 Console.WriteLine($"{a} возведенное в степень {b} равно {exp(a,b)}");
 
+//Задача 28: Подсчитайте сумму цифр в числе.
+Console.WriteLine();
+Console.WriteLine("Задача 28: Подсчитайте сумму цифр в числе.");
+Console.WriteLine();
+int var28 = new Random().Next(1,1000);
+int summary(int num) {
+    if (num<10) {
+        return num;
+    } else return num%10+summary(num/10);
+}
+Console.WriteLine($"Для числа {var28} сумма числел равна {summary(var28)}");
