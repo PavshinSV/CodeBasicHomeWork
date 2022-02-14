3895388863d323b4205ccf731dc7dcae2d5a8206
PSV <pavshin@mail.ru>
2022-02-14 19:37:25 +0700

    Task N32 - Solved

--- PracticeFour/Program.cs
+++ PracticeFour/Program.cs
@@ -5,9 +5,12 @@
 Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
 Задача 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран */
 
-Console.Clear();
+
 
 //Задача 26: Возведите число А в натуральную степень B, используя цикл.
+
+Console.Clear();
+
 Console.WriteLine();
 Console.WriteLine("Задача 26: Возведите число А в натуральную степень B, используя цикл.");
 Console.WriteLine();
@@ -34,3 +37,35 @@ int summary(int num) {
     } else return num%10+summary(num/10);
 }
 Console.WriteLine($"Для числа {var28} сумма числел равна {summary(var28)}");
+
+//Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
+Console.WriteLine();
+Console.WriteLine("Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.");
+Console.WriteLine();
+int limit = 100;
+for (int index = 2; index < limit; index += 2) {
+    Console.WriteLine($"Третья степень числа {index} равна {exp(index,3)}");
+}
+Console.WriteLine();
+
+//Задача 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран 
+Console.WriteLine();
+Console.WriteLine("Задача 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран");
+Console.WriteLine();
+int[] array = new int[8];
+void FillArray01(int[] arr){
+    for (int i=0; i<arr.Length; i++) {
+        arr[i]=new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] arr) {
+    Console.WriteLine();
+    for (int i=0; i<arr.Length; i++) {
+        Console.Write($"а[{i}] = {arr[i]}; ");
+    }
+    Console.WriteLine();
+}
+
+FillArray01(array);
+PrintArray(array);
