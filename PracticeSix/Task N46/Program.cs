fec84972a8173f678303acbb1b56143fb8a58866
PSV <pavshin@mail.ru>
2022-02-21 07:30:32 +0700

    46 - debug

--- PracticeSix/Task N46/Program.cs
+++ PracticeSix/Task N46/Program.cs
@@ -9,4 +9,88 @@
 
  */
 
- 
+Console.Clear();
+Console.WriteLine();
+Console.WriteLine("Задача 46: Написать программу масштабирования фигуры.");
+Console.WriteLine();
+Console.WriteLine("Введите в одну строку координаты вершин фигуры. Координаты должны быть целыми числами разделенные запятой. Наличие пробелов при вводе чисел - непринципиально.");
+Console.WriteLine("Пример ввода вершин треугольника: (0,0) ( 25 , 18 ) (20, -7)");
+Console.WriteLine("Количество вводимых вершин и значность (разрядность) чисел не имеет значения");
+Console.WriteLine();
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i != arr.Length - 1) { Console.Write("; "); }
+    }
+    Console.WriteLine("]");
+}
+
+void PrintArray2(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(0); j++)
+    {
+        Console.Write($"N{j + 1} - [");
+        for (int i = 0; i < arr.GetLength(1); i++)
+        {
+            Console.Write($"{arr[j, i]}");
+            if (i != arr.GetLength(1) - 1) { Console.Write("; "); }
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] Scale(int[] arr, double k)
+{
+    double[,] swap = new double[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            swap[i, j] = arr * k;
+
+        }
+    }
+}
+
+int[,] GetCoordinates(string[] spool)
+{
+    string[] swap = new string[2];
+    int[,] coordinates = new int[spool.Length, 2];
+    for (int i = 0; i < spool.Length; i++)
+    {
+        swap = spool[i].Split(',');
+        coordinates[i, 0] = int.Parse(swap[0]);
+        coordinates[i, 1] = int.Parse(swap[1]);
+    }
+    return coordinates;
+}
+
+string inputString = Console.ReadLine();
+
+string swap = String.Empty;
+for (int i = 0; i < inputString.Length; i++)
+{
+    if (inputString[i] != ' ')
+    {
+        swap += inputString[i];
+    }
+}
+
+string[] arrayOfWords = swap.Split(new[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+PrintArray(arrayOfWords);
+
+int[,] arrayOfCoordinates = GetCoordinates(arrayOfWords);
+
+Console.WriteLine("Вы ввели следующие координаты");
+PrintArray2(arrayOfCoordinates);
+Console.WriteLine();
+
+Console.WriteLine("Введите коэфициент масштабирования:");
+double scale = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+double[,] scaledArray = Scale(arrayOfCoordinates, scale);
