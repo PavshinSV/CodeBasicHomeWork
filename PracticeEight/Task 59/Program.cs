8c3049d10e1ba60a398e32cdb8b2e484f0a0782b
PSV <pavshin@mail.ru>
2022-03-06 10:40:12 +0700

    Task N59 - Solved

--- PracticeEight/Task 59/Program.cs
+++ PracticeEight/Task 59/Program.cs
@@ -1,2 +1,59 @@
 ﻿// Задача 59: В прямоугольной матрице найти строку с наименьшей суммой элементов.
 
+
+Console.Clear();
+
+void ArrayMinLineSumm(int[,] arr)
+{
+    int indexOfMinSummLine = 0;
+    int minSumm=0;
+    int summ;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+       summ=0;
+       for (int j = 0; j < arr.GetLength(1); j++)
+       {
+           summ+=arr[i,j];
+       }
+       if (i==0){minSumm=summ;}
+       else if (minSumm>summ){
+           minSumm=summ;
+           indexOfMinSummLine=i;
+       };
+    }
+    Console.WriteLine($"В указанной матрице наименьшей суммой = {minSumm} обладает строка с индексом {indexOfMinSummLine}");
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write("{0,5}",arr[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int lines = 5;
+int columns = lines;
+int[,] testArray = new int[lines, columns];
+
+FillArray(testArray);
+Console.WriteLine("На старте имеем несортированный массив:");
+PrintArray(testArray);
+Console.WriteLine();
+ArrayMinLineSumm(testArray);
+Console.WriteLine();
