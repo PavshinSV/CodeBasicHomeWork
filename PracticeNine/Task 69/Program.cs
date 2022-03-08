2e33d52f47e40206c9f73613dc48cdd1e965afec
PSV <pavshin@mail.ru>
2022-03-08 17:15:15 +0700

    Task N69 - Solved

--- PracticeNine/Task 69/Program.cs
+++ PracticeNine/Task 69/Program.cs
@@ -4,3 +4,21 @@ M = 1; N = 4 -> 10
 
 M = 4; N = 8. -> 30 */
 
+Console.Clear();
+Console.WriteLine();
+
+int ShowSummNatural(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return m + ShowSummNatural(m + 1, n);
+}
+
+int m = 4;
+int n = 8;
+
+Console.WriteLine($"В диапазоне от M = {m} до N = {n} сумма всех чисел равна:");
+Console.WriteLine(ShowSummNatural(m, n));
+Console.WriteLine();
