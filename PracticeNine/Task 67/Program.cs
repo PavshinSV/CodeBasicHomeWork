24f1d590443e284dbda4cf1c55750e31cde21d94
PSV <pavshin@mail.ru>
2022-03-08 17:02:01 +0700

    Task N67 - Solved

--- PracticeNine/Task 67/Program.cs
+++ PracticeNine/Task 67/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Задача 67: Показать натуральные числа от N до 1, N задано.
 
+Console.Clear();
+
+void ShowNatural(int n) {
+    if (n==0) {
+        return;
+    }
+    Console.Write($"{n} ");
+    ShowNatural(n-1);
+}
+
+Console.WriteLine();
+
+int n=15;
+Console.WriteLine($"Для введенного числа N = {n} все натуральные числа представлены рядом:");
+ShowNatural(n);
+Console.WriteLine();
